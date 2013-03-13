d66854d0b5975e7dbe5dd5257ffa97e658f0749e
Chase Florell <github@chaseflorell.com>
2013-03-13 17:39:44 -0600

    added dot-source-external-scripts.ps1

---
+++ dot-source-external-scripts.ps1
@@ -0,0 +1,4 @@
+﻿# I found this to be a more reliable way of DOT SOURCING external scripts.
+
+$ScriptDirectory = Split-Path $MyInvocation.MyCommand.Path
+. (Join-Path $ScriptDirectory some-script.ps1)
