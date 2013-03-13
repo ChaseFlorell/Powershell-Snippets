8912dbc57c1750105f1820254bd6cdb9af624ccc
Chase Florell <github@chaseflorell.com>
2013-03-13 17:44:57 -0600

    added regions-to-aid-with-collapsible-code.ps1

---
+++ regions-to-aid-with-collapsible-code.ps1
@@ -0,0 +1,10 @@
+﻿#region This is a collapseable region
+
+    # you can put code in here and have it collapse
+    # NOTE: some IDEs/ISEs don't honor the region, but the good ones do.
+
+    #These ISEs are known to work.
+        # Powershell ISE
+        # PowerGUI
+
+#endregion
